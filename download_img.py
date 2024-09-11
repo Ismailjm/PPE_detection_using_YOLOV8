b409e7baaaef1f3c2e91e69db4a6e5da601fd355
Ismail EL JAMIY <104994203+Ismailjm@users.noreply.github.com>
2024-09-11 19:08:48 +0200

    Update download_img.py

--- download_img.py
+++ download_img.py
@@ -2,5 +2,6 @@ from simple_image_download import simple_image_download as simp
 response = simp.simple_image_download
 
 keywords = ["laborer","worker"]
+# download images of ["laborer","worker"]
 for kw in keywords:
     response().download(kw,200) 
